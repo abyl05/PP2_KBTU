e3b071aa2823591182d255abbf9bc8ff2dd91acd
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-01-30 23:55:21 +0600

    Variables

---
+++ practice1/Variables.py
@@ -0,0 +1,20 @@
+abs, ab_cd = "ab", "cd"
+_a_b, aBc, ABC = "ef", "gh", "ij"
+abd, mza, qo = 12, 34, 56
+a = b = c = d = "Almaty"
+cities = ["Atray", "Aktay", "Semei"]
+z, x, y = cities
+print(z)
+print(y)
+print(a); print(b); print(c); print(d)
+print(abs + _a_b); print(abd + mza); print(ab_cd, qo); print(a, abd)
+
+def myfunc():
+  x = "family"
+  print("Love is " + x); print(z)
+  print("Love is " + y); print(d); print(abs)
+myfunc()
+
+print(x)
+
+
