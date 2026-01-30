598303696df91cda91245b4bc463acb1271afb2e
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-01-31 03:38:37 +0600

    String_5,6

---
+++ practice1/String_5,6.py
@@ -0,0 +1,33 @@
+age = 36
+t = f"My name is May, I am {age}"
+print(t)
+age = 12
+t1 = f"My name is John, I am {age}"
+print(t1)
+age = 23
+tx = f"My name is Mike, I am {age}"
+print(tx)
+age = 70
+tx1 = f"My name is Kevin, I am {age}"
+print(tx1)
+age = 57
+txt = f"My name is Dave, I am {age}"
+print(txt)
+
+q = f"The price is {20 * 5} dollars"
+print(q)
+w = f"The price is {2 * 59} dollars"
+print(w)
+e = f"The price is {2 * 9} dollars"
+print(e)
+r = f"The price is {90 * 59} dollars"
+print(r)
+y = f"The price is {4 * 8} dollars"
+print(y)
+
+z = "It’s the \"tip\" of the iceberg."
+x = "Learn to walk \"before\" you run."
+c = "Don’t bite off \"more than\" you can chew."
+v = "It’s better to be \"safe\" than sorry."
+b = "The \"early\" bird catches the worm."
+print(z); print(x); print(c); print(v); print(b)
