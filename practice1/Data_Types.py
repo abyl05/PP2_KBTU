ca30bc9468d5b2ce91e621e26e6eb6b41f13b0fb
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-01-31 00:02:31 +0600

    Data_Types

---
+++ practice1/Data_Types.py
@@ -0,0 +1,14 @@
+z = 5
+x = 'X'
+c = "almaty"
+v = 3.14
+b = ["apple", "banana", "orange", "mango"]
+n = True
+m = bytearray(5)
+print(type(z))
+print(type(x))
+print(type(c))
+print(type(v))
+print(type(b))
+print(type(n))
+print(type(m))
