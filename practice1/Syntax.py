653f85d547b2fa5461b41e0a1f521661c02a4a6f
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-01-30 23:26:13 +0600

    Syntax

---
+++ practice1/Syntax.py
@@ -0,0 +1,20 @@
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+      print("Five is greater than two!")
+if 5 > 2:
+                print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+if 5 > 2:
+            print("Five is greater than two!") #can write print any were 
+a = 1
+b = "Almaty" #also variable can be a string
+c = 3.14  #also variable can be a float
+d = 'C'  #also variable can be a char
+e = 123349762347  #also variable can be an any number
+
+print(a); print(e)
+print("Pi number = "); print(c)
+print(b); print(" is my lovely town.")
+print(d); print(" is 3-rd letter in alphabet.")
