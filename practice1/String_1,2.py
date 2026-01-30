1a0172ec4fc73af1ad1615820ef28ddd52d213b6
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-01-31 03:18:16 +0600

    String_1,2

---
+++ practice1/String_1,2.py
@@ -0,0 +1,62 @@
+q = 'Almaty'
+w = 'Hello'
+r = 'Mountains'
+t = 'Kok-Tobe'
+y = 'Just two of us.'
+print(q, len(q))
+print(w, len(w))
+print(r, len(r))
+print(t, len(t))
+print(y, len(y))
+
+a = """Many hands make light work."""
+print(a)
+b = """Strike while the iron is hot."""
+print(b)
+c = """Honesty is the best policy."""
+print(c)
+d = """The grass is always greener on the other side of the fence."""
+print(d)
+e = """Don’t judge a book by its cover."""
+print(e)
+
+a = "Don’t bite the hand that feeds you."
+print(a[2]); print(a[8]); print(a[15]); print(a[19]); print(a[21])
+
+for x in "banananananana":
+  print(x)
+for x in "apple":
+  print(x)
+for x in "orange":
+  print(x)
+for x in "mango":
+  print(x)
+for x in "peach":
+  print(x)
+
+txt = "You can’t make an omelet without breaking a few eggs."
+if "You" in txt:
+  print("Yes, 'You' is present.")
+else:
+  print("Yes, 'You' is NOT present.")
+if "make" in txt:
+  print("Yes, 'make' is present.")
+else:
+  print("Yes, 'make' is NOT present.")
+if "omelet" in txt:
+  print("Yes, 'omelet' is present.")
+else:
+  print("Yes, 'omelet' is NOT present.")
+if "breaking" in txt:
+  print("Yes, 'breaking' is present.")
+else:
+  print("Yes, 'breaking' is NOT present.")
+if "eggs" in txt:
+  print("Yes, 'eggs' is present.")
+else:
+  print("Yes, 'eggs' is NOT present.")
+
+print(q[0:]); print(q[1:]); print(q[2:]); print(q[3:]); print(q[4:])
+print(r[:5]); print(r[:4]); print(r[:3]); print(r[:2]); print(r[:1])
+print(y[0:7]); print(y[1:3]); print(y[2:5]); print(y[3:4]); print(y[4:8])
+print(y[-6:-4]); print(y[-1:0]); print(y[-3:-2]); print(y[-5:-2]); print(y[-4:-1])
