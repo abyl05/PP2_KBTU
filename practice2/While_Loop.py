313ce9676c6fe0791effc0297e877b118637b23f
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-02-07 01:13:14 +0600

    While_Loop

---
+++ practice2/While_Loop.py
@@ -0,0 +1,100 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+q = 2
+while q < 10:
+    print(q)
+    q += 2
+w = 5
+while w > 0:
+    print(w)
+    w -= 1
+e = 1
+while e <= 5:
+    print(e * e)
+    e += 1
+r = 1
+while r < 6:
+    print("Number:", r)
+    r += 1
+
+
+a = 1
+while a < 6:
+    if a == 4:
+        break
+    print(a)
+    a += 1
+a = 1
+while True:
+    print(s)
+    if s == 3:
+        break
+    s += 1
+s = 1
+while d < 10:
+    print(d)
+    if d > 7:
+        break
+    d += 1
+while True:
+    num = int(input("Enter a number: "))
+    if num == 0:
+        break
+    print("You entered:", num)
+f = 1
+total = 0
+while True:
+    total += f
+    if total >= 10:
+        break
+    f += 1
+
+
+z = 0
+while z < 5:
+    z += 1
+    if z == 3:
+        continue
+    print(z)
+x = 0
+while x < 10:
+    x += 1
+    if x % 2 == 0:
+        continue
+    print(x)
+c = 0
+while c < 10:
+    c += 1
+    if c == 5:
+        continue
+    print(c)
+numbers = [3, -1, 5, -2, 7]
+v = 0
+while v < len(numbers):
+    if numbers[i] < 0:
+        v += 1
+        continue
+    print(numbers[v])
+    v += 1
+b = 0
+while b < 10:
+    b += 1
+    if b % 3 == 0:
+        continue
+    print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
