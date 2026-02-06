2b04df4c4273de170780cf76e88ecf7099f5aeb2
Abylaikhan Temirzhanov <abylaikhantemirzhanov@gmail.com>
2026-02-07 00:43:20 +0600

    Operators

---
+++ practice2/Operators.py
@@ -0,0 +1,42 @@
+print(10 + 5)
+print(10 + 5)
+print(10 + 5)
+print(10 + 5)
+print(10 + 5)
+sum1 = 100 + 50      
+sum2 = sum1 + 250    
+sum3 = sum2 + sum2 
+sum4 = 10 + 2      
+sum5 = sum4 + 123    
+sum6 = sum5 + sum5 
+sum7 = 40 + 90      
+sum8 = sum7 + 25    
+sum9 = sum8 + sum8 
+sum10 = 45 + 324      
+sum11 = sum10 + 980    
+sum12 = sum11 + sum11 
+
+x, z, c, v = 15, 16, 17, 18
+y, q, w, e = 4, 5, 6, 7
+print(x + y); print(z + q); print(c + w); print(v + e); 
+print(x - y); print(z - q); print(c - w); print(v - e); 
+print(x * y); print(z * q); print(c * w); print(v * e); 
+print(x / y); print(z / q); print(c / w); print(v / e); 
+print(x % y); print(z % q); print(c % w); print(v % e); 
+print(x ** y); print(z ** q); print(c ** w); print(v ** e); 
+print(x // y); print(z // q); print(c // w); print(v // e); 
+
+p = 100
+p+=10; p+=34; p+=92; p+=63; p+=72
+p-=10; p-=34; p-=92; p-=63; p-=72 
+p*=10; p*=34; p*=92; p*=63; p*=72
+p/=10; p/=34; p/=92; p/=63; p/=72
+p%=10; p%=34; p%=92; p%=63; p%=72
+p//=10; p//=34; p//=92; p//=63; p//=72
+p**=10; p**=34; p**=92; p**=63; p**=72
+p&=10; p&=34; p&=92; p&=63; p&=72
+p|=10; p|=34; p|=92; p|=63; p|=72
+p^=10; p^=34; p^=92; p^=63; p^=72 
+p>>=10; p>>=34; p>>=92; p>>=63; p>>=72 
+p<<=10; p<<=34; p<<=92; p<<=63; p<<=72
+ 
