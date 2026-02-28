15802612b5b90f7d2609d86b2cc46ff2c80549f4
abyl05 <abylaikhantemirzhanov@gmail.com>
2026-02-28 12:40:37 +0500

    Delete t3.py

--- t3.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-now = datetime.now()
-without_microseconds = now.replace(microsecond=0)
-
-print("Original:", now)
-print("Without microseconds:", without_microseconds)
